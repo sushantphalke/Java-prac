d4bcb523cf40f82ec7dba3f1fadcfb103dbb5140
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-06 22:09:33 +0530

    if statement tried

---
+++ Output.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Output {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        String sush = name;
+        if (name == name){
+            System.out.println(name);
+
+        }
+        System.out.println(name);
+
+
+    }
+}
