617c13eea75fe67497ce3f6eb53caf13b5525ca1
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-09 11:48:21 +0530

    strings length and char at i

--- Strings.java
+++ Strings.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Strings {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("enter your name ");
         String name = input.nextLine();
         System.out.println("your name is "+ name);
+        System.out.print("enter your fullname ");
+        String fullname = input.nextLine();
+        System.out.println("your name is "+ fullname);
+//        printing character at position
+        for (int i =  0 ;i<fullname.length();i++){
+            System.out.println(fullname.charAt(i));
+        }
+
+
     }
 }
