6fcb1d7e16d4d5eb4856eb57b080fa945ae5be12
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-09 11:13:33 +0530

    strings in java

---
+++ Strings.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("your name is "+ name);
+    }
+}
