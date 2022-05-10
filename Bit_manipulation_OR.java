d7dc2086940f5f90f918520b266e6be0c7da0663
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-10 22:57:07 +0530

    bit manipulation OR operation

--- Bit_manipulation_OR.java
+++ Bit_manipulation_OR.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Bit_manipulation_OR {
     public static void main(String[] args) {
-        int number = 5;
-        int pos = 2;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : " );
+        int number = input.nextInt();
+        System.out.print("Enter the shift position : ");
+        int pos = input.nextInt();
         int bitmask = 1 << pos;
 
         int newNumber = number | pos;
-        System.out.println(newNumber);
+
+        System.out.println("Number got with OR operation after position shift of : "+pos+" is : "+newNumber);
     }
 }
