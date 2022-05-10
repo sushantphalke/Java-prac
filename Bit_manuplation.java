c71a73ec80c9711be2455abde2e8a9303ed77048
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-10 21:05:27 +0530

    bit at specific position +print binary of given number

--- Bit_manuplation.java
+++ Bit_manuplation.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Bit_manuplation {
     public static void main(String[] args) {
 
 //      Get the bit at specific position ****************
-        int number = 5;
-        int position = 2;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = input.nextInt();
+        System.out.print("Enter the position to know bit: ");
+        int position = input.nextInt();
         int bitmask =  1<<position;
         if ((bitmask & number) == 0){
             System.out.println("bit at position : "+position+" of number : "+number+" is 0");
@@ -12,5 +17,16 @@ public class Bit_manuplation {
             System.out.println("bit at position : "+position+" of number : "+number+" is 1");
 
         }
+//      **********************
+
+        System.out.println("* Bits are counted from Left >> Right * and BINARY OF "+ number +" is : "+toBinary(number,32));
+    }
+    public static String toBinary(int n, int len)
+    {
+        String binary = "";
+        for (long i = (1L << len - 1); i > 0; i = i / 2) {
+            binary += (n & i) != 0 ? "1" : "0";
+        }
+        return binary;
     }
 }
