8b81e8c8bfd39e5283b2e6a65179c2c5c77caf1e
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-10 23:37:05 +0530

    set bit and clear bit operation

--- Bit_manipulation_OR.java
+++ Bit_manipulation_OR.java
@@ -9,14 +9,19 @@ public class Bit_manipulation_OR {
         System.out.print("Enter the shift position : ");
         int pos = input.nextInt();
         int bitmask = 1 << pos;
+        System.out.print("Enter the operation type 1 for set bit or 0 for clear bit : ");
+        int operationtype = input.nextInt();
+        if (operationtype ==1) {
 
-        int newNumber = number | pos;
 
-        System.out.println("Number got with OR(set bit) operation after position shift of : "+pos+" is : "+newNumber);
+            int newNumber = number | pos;
+
+            System.out.println("Number got with OR(set bit) operation after position shift of : " + pos + " is : " + newNumber);
+        }else {
 //      clear bit - set bit to 0
-        int clearedbitnumber = number & ~bitmask;
-        System.out.println("Number got with AND & NOT bitmask(clear bit) operation after position shift of : "+pos+" is : " + clearedbitnumber);
+            int clearedbitnumber = number & ~bitmask;
+            System.out.println("Number got with AND & NOT bitmask(clear bit) operation after position shift of : " + pos + " is : " + clearedbitnumber);
 
-
+        }
     }
 }
