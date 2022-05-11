1696b07348e6bc26fe42801096101bcb754db429
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-11 19:52:52 +0530

    insertion  sort

---
+++ Insertion_sort.java
@@ -0,0 +1,18 @@
+public class Insertion_sort {
+    public static void main(String[] args) {
+        int [] numbers = {7,3,4,2,5,1};
+
+        for (int i=1; i< numbers.length;i++){
+            int current = numbers[i];
+            int j=i-1;
+            while(j>=0 && current<numbers[j]){
+                numbers[j+1]=numbers[j];
+                j--;
+            }
+            numbers[j+1]=current;
+        }
+        for(int i =0 ;i<(numbers.length);i++){
+            System.out.print(numbers[i]+"  ");
+        }
+    }
+}
