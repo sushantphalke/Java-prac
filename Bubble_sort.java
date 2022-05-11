5de8af8ea07d124d2559c25bec92f96597f77020
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-11 12:51:45 +0530

    arrays for bubblr sort

---
+++ Bubble_sort.java
@@ -0,0 +1,12 @@
+public class Bubble_sort {
+    public static void main(String[] args) {
+        int [] numbers = new int[5];
+        for(int i =0 ;i<(numbers.length);i++){
+            numbers[i]=i;
+        }
+        for(int i =0 ;i<(numbers.length);i++){
+            System.out.print(numbers[i]+"  ");
+        }
+
+    }
+}
