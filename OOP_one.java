e5e56ed8f0cd80c243ad880e9fc9c3fe885ff9cd
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-12 15:35:13 +0530

    class and obj with this method

--- OOP_one.java
+++ OOP_one.java
@@ -2,22 +2,36 @@ class Pen {
 
     StringBuilder name = new StringBuilder("");
     StringBuilder type = new StringBuilder(""); // ballpoint,gel,etc.
+    StringBuilder color = new StringBuilder("");
 
-    public void Write(){
-        System.out.println("Writing something");
+    public void PrintProperties(){
+// this method calls properties of given objects
+        System.out.println("name: "+this.name);
+        System.out.println("type: "+this.type);
+        System.out.println("color: "+this.color);
     }
 }
 
 
 public class OOP_one {
     public static void main(String[] args) {
+
         Pen pen1 = new Pen();
-//      in below code pen1.name is assigned by stringBuilder but for printing output it taken as string by new string
+        pen1.name= new StringBuilder("Parker");
+        pen1.type= new StringBuilder("Ballpoint");
+        pen1.color= new StringBuilder("Blue");
+        pen1.PrintProperties();
+
+
+/*      in below code pen1.name is assigned by stringBuilder but for printing output it taken as string by new string
         String pen1name = new String(pen1.name= new StringBuilder("Parker"));
         String pen1type = new String(pen1.type= new StringBuilder("Ballpoint"));
-//      System.out.println(pen1.name.charAt(3));
+        System.out.println(pen1.name.charAt(3));
         System.out.println(pen1name + pen1type);
-        pen1.Write();
+*/
 
-    }
+
+
+
+    }//end of psvm
 }
