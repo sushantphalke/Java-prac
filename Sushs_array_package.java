4b185d0b29c575f2a492d2f541e7bea77a739a09
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-13 15:48:49 +0530

    sush-array-package update

--- Sushs_array_package.java
+++ Sushs_array_package.java
@@ -26,4 +26,73 @@ public class Sushs_array_package {
 
 
 
+//    Sorting algorithms
+
+    //bubble sort function
+    public static int[] bubble_sort(int[] arrayToSort){
+        for (int i = 0; i<arrayToSort.length-1; i++){
+            for (int j=0;j<arrayToSort.length-i-1;j++) {
+
+                if (arrayToSort[j] > arrayToSort[j + 1]) {
+                    int temp = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j + 1];
+                    arrayToSort[j + 1] = temp;
+                }
+            }
+        }
+
+        return arrayToSort;
+    }
+
+    //insertion sort function
+    public static int[] insertion_sort(int[] arrayToSort){
+        for (int i=1; i< arrayToSort.length;i++){
+            int current = arrayToSort[i];
+            int j=i-1;
+            while(j>=0 && current<arrayToSort[j]){
+                arrayToSort[j+1]=arrayToSort[j];
+                j--;
+            }
+            arrayToSort[j+1]=current;
+        }
+
+        return arrayToSort;
+    }
+
+    //selection sort function
+    public static int[] selection_sort(int[] arrayToSort){
+
+        for (int i = 0; i<arrayToSort.length; i++){
+
+//            int noToSwap = arrayToSort[i];
+//            int position = i;
+//
+//            for (int j=i+1;j<arrayToSort.length;j++) {
+//
+//                if (noToSwap>arrayToSort[j]) {
+//                    noToSwap = arrayToSort[j];
+//                    position= j;
+//                }
+//            }
+//            int temp = arrayToSort[i];
+//            arrayToSort[i]=arrayToSort[position];
+//            arrayToSort[position]=temp;
+
+            int smallest = i;
+            for (int j=i+1; j<arrayToSort.length;j++){
+                if(arrayToSort[smallest]>arrayToSort[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arrayToSort[smallest];
+            arrayToSort[smallest] = arrayToSort[i];
+            arrayToSort[i] = temp;
+        }
+
+        return arrayToSort;
+    }
+
+
+
+
 }
