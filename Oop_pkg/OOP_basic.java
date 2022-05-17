86280e39983baf0c4948f9d0740286e8a8738519
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-17 09:21:06 +0530

    interface implemented

--- Oop_pkg/OOP_basic.java
+++ Oop_pkg/OOP_basic.java
@@ -1,7 +1,21 @@
 package Oop_pkg;
+interface Animal {
+    int eys = 2;
+    void walk();
+
+}
+interface Herbivore {
+
+}
+class Horse implements Animal,Herbivore{
+    public void walk() {
+        System.out.println("walks on 4 legs");
+    }
+}
 public class OOP_basic {
     public static void main(String[] args) {
-
+    Horse horse = new Horse();
+    horse.walk();
     }
 
 
