0225a20e018cb77bcbd51feda380b457505b0d5e
sushantphalke <77383095+SushantPhalke@users.noreply.github.com>
2022-05-17 21:55:01 +0530

    recursion

--- Recursion_one.java
+++ Recursion_one.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Recurssion_one {
+public class Recursion_one {
+    public static void main(String[] args) {
+//      calculating sum of n numbers using recursion
+      int sum =0;
+      int n = 100;
+        System.out.println(Calculate_Sum(n));
+
+    }
+//    sum of n positive integers using recursion
+    public static int Calculate_Sum( int n){
+       if(n>0) return (n + Calculate_Sum(n - 1));
+       else {
+           return 0;
+       }
+    }
+
 }
